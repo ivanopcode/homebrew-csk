c7ae1d18de7958a0a656720a9c9fcd14be5885fa
Ivan Oparin <oparin@me.com>
2026-05-19 23:21:41 +0400

    Bump cocoaskills to 0.5.0

--- Formula/cocoaskills.rb
+++ Formula/cocoaskills.rb
@@ -3,8 +3,8 @@ class Cocoaskills < Formula
 
   desc "Local skill manager for AI agent skills"
   homepage "https://github.com/ivanopcode/cocoaskills"
-  url "https://github.com/ivanopcode/cocoaskills/releases/download/v0.4.0/cocoaskills-0.4.0.tar.gz"
-  sha256 "0771bdac180861fb38f9ab40327b77ec3027fd3d1105b175adb6fc55c30f3246"
+  url "https://github.com/ivanopcode/cocoaskills/releases/download/v0.5.0/cocoaskills-0.5.0.tar.gz"
+  sha256 "74b6815e081b57e5d00299f6d8d183822be3472643900358a343c478e37c7235"
   license "Apache-2.0"
 
   depends_on "python@3.13"
